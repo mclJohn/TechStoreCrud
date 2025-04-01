8b3928bc48dfaaf5eaccbae5c82b58b33d88dfc5
John Victor <101483364+mclJohn@users.noreply.github.com>
2025-03-31 21:50:07 -0300

    Update .sql

--- .sql
+++ .sql
@@ -1,10 +1,9 @@
 -- Criação do banco de dados 'usuarios'
 CREATE DATABASE IF NOT EXISTS `usuarios`;
 
--- Usando o banco de dados 'usuarios'
+
 USE `usuarios`;
 
--- Criação de uma tabela exemplo 'clientes'
 CREATE TABLE IF NOT EXISTS `clientes` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(100) NOT NULL,
@@ -12,7 +11,6 @@ CREATE TABLE IF NOT EXISTS `clientes` (
     PRIMARY KEY (`id`)
 );
 
--- Inserção de dados na tabela 'clientes'
 INSERT INTO `clientes` (`nome`, `email`) VALUES
 ('João Silva', 'joao@email.com'),
 ('Maria Oliveira', 'maria@email.com');
